edaa9274171c239dc4a1588194f83fa5cc99bb15
Chussya <mr.oldyrev@gmail.com>
2026-06-03 22:43:53 +0300

    [#5]feat: added static and dynamic linking

--- RoguelikeGame/GameMain.cpp
+++ RoguelikeGame/GameMain.cpp
@@ -2,10 +2,25 @@
 #include "Application.h"
 #include "Engine.h"
 
+// For engine test
+//#include <iostream>
+//#include "windows.h"
+
 using namespace RoguelikeGame;
 
 int main()
 {
+	// For engine test
+	//if (AllocConsole())
+	//{
+	//	FILE* fp;
+	//	freopen_s(&fp, "CONOUT$", "w", stdout);
+	//	freopen_s(&fp, "CONOUT$", "w", stderr);
+	//} else
+	//{
+	//	std::cerr << "Console error..." << std::endl;
+	//}
+
 	Engine engine;
 	engine.init();
 	engine.run();
